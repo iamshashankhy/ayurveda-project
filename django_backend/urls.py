5ea65f90408b3ba152222bc7efd2cc1ea399f7ed
Shashankgowda <shashankyadukumar@gmail.com>
2025-12-04 17:15:00 +0530

    Add files via upload

--- django_backend/urls.py
+++ django_backend/urls.py
@@ -1,15 +1,20 @@
 from django.urls import path
-from rest_framework_simplejwt.views import (
-    TokenObtainPairView,
-    TokenRefreshView,
+from .views import (
+    AnalyzeDoshaView,
+    PredictCancerView,
+    CombinedAssessmentView,
+    AssessmentQuestionsView,
+    ProfileView,
+    ReviewsView,
 )
-from .views import RegisterView
 
 urlpatterns = [
-    path('register/', RegisterView.as_view(), name='register'),
-    path('register', RegisterView.as_view(), name='register-no-slash'),
-    path('login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('login', TokenObtainPairView.as_view(), name='token_obtain_pair-no-slash'),
-    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    path('token/refresh', TokenRefreshView.as_view(), name='token_refresh-no-slash'),
+    path('analyze_dosha/', AnalyzeDoshaView.as_view(), name='analyze_dosha'),
+    path('predict_cancer/', PredictCancerView.as_view(), name='predict_cancer'),
+    path('assessment/combined/', CombinedAssessmentView.as_view(), name='combined_assessment'),
+    path('assessment/questions/', AssessmentQuestionsView.as_view(), name='assessment_questions'),
+    path('profile/', ProfileView.as_view(), name='profile'),
+    path('reviews/', ReviewsView.as_view(), name='reviews'),
 ]
+
+
