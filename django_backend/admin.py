e5c88c50af145509498ed9c239ffcac2c7a7c73e
Shashankgowda <shashankyadukumar@gmail.com>
2025-12-04 17:13:36 +0530

    Add files via upload

--- django_backend/admin.py
+++ django_backend/admin.py
@@ -1,10 +1,23 @@
 from django.contrib import admin
-from .models import Profile
+from .models import DoshaEvaluation, CancerRiskResult, ActivityLog
 
-@admin.register(Profile)
-class ProfileAdmin(admin.ModelAdmin):
-    list_display = ('user', 'created_at', 'updated_at')
+@admin.register(DoshaEvaluation)
+class DoshaEvaluationAdmin(admin.ModelAdmin):
+    list_display = ('user', 'vata', 'pitta', 'kapha', 'created_at')
+    list_filter = ('created_at',)
     search_fields = ('user__username', 'user__email')
+
+@admin.register(CancerRiskResult)
+class CancerRiskResultAdmin(admin.ModelAdmin):
+    list_display = ('user', 'risk', 'probability', 'created_at')
+    list_filter = ('risk', 'created_at')
+    search_fields = ('user__username', 'user__email')
+
+@admin.register(ActivityLog)
+class ActivityLogAdmin(admin.ModelAdmin):
+    list_display = ('user', 'action', 'created_at')
+    list_filter = ('action', 'created_at')
+    search_fields = ('user__username', 'user__email', 'action')
 
 
 
